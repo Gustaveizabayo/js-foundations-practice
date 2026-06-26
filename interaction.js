c1da178bf3b8e431bf8c23be93cdf35680421749
IZABAYO Gustave <izabayogustave54@gmail.com>
2026-06-26 19:09:18 +0200

    interaction

---
+++ interaction.js
@@ -0,0 +1,8 @@
+alert("Hello");
+
+//result = prompt(title, [default]);
+let age = prompt ('How old are you?', 100);
+alert(`You are ${age} yers old!`);
+
+result = confirm("Are you the boss?");
+alert (isBoss); //true if ok is pressed
